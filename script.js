acb8bc4944e4a250efe3d0cb1b2be0b78a13f485
Gordon Zhou <993192@learn.vsb.bc.ca>
2021-11-22 14:56:56 -0800

    started javascript portion

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function GatherData() {
+    let firstName = document.getElementById("fname").value;
+    let lastName =  document.getElementById("lname").value;
+    let birthday = document.getElementById("bday").value;
+    let email = document.getElementById("email").value;
+    let phoneNumber = document.getElementById("phone").value;
+    let password = document.getElementById("pwd").value;
+    console.log("dfd");
+    console.log(firstName, lastName, birthday, email, phoneNumber, password);
+}
