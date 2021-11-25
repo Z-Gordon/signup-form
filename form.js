0f9a58777a421b5ea0c73f69df954edfc1fdb611
Gordon Zhou <993192@learn.vsb.bc.ca>
2021-11-25 14:52:22 -0800

    FINAL COMMIT

---
+++ form.js
@@ -0,0 +1,22 @@
+clicksBeforeMsg = 1
+cooldown = 0
+
+function doScam() {
+    let msgs = [`Critical alert from Microsoft. Your computer has alerted us. Malicious Pornographic Spyware/Riskware Detected Error #xxxxxxxxx Please call us immediately at 1-800-000-0000 Do not ignore this critical alert If you close this page, your computer will be blocked to prevent further damage to our network, and hard drive may possibly be deleted.
+    The following information is at risk: 
+        ** Financial Data 
+        ** Account logins and passwords 
+        ** Photos and other personal files
+    DO NOT PRESS ANY KEY UNLESS YOU ARE INSTRUCTED TO DO SO TOLL FREE 1-800-000-0000`, 
+    "* Thank 🙌 you 👈 for using 😏 .T. Mobile 📲 . Your 👉 recent 🕑 purchase 🛒 has got 🉐 you 👉 eligible 😱 for an exclusive 💂 reward 💰 worth 💸 $100 😆😂😅. You 👈 are one ☝🙏🏼 of 10 🔟 randomly 🎲 selected 👇 .T. Mobile 📱 customers 💩 who will receive 😩 this reward 💰. You 😀😬👈 have been rewarded 🎁 a free 🆓 reward 🎁🅱 worth 💰 $100 💯. Just click 🖱 on 🔛🏽 the button 🔘 below 🙆 to claim 🌊 your 🍑👉 reward 💰."]
+    alert(msgs[Math.floor(Math.random() * msgs.length)]);
+}
+
+document.body.addEventListener("click", function (evt) {
+    if (cooldown == 0){
+        doScam();
+    }
+    else {
+        cooldown--;
+    }
+});
